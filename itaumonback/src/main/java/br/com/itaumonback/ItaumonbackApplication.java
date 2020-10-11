fcbc42fda32f19ffcc184df85758388062c57250
romevas <rogrock666@hotmail.com>
2020-10-11 15:13:44 -0300

    inicial

---
+++ itaumonback/src/main/java/br/com/itaumonback/ItaumonbackApplication.java
@@ -0,0 +1,13 @@
+package br.com.itaumonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaumonbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaumonbackApplication.class, args);
+	}
+
+}
